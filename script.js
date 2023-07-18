d92a7a91f241dae5d55f4b80640187d2f398a673
Jeff Ramos <jeramos409@gmail.com>
2023-07-18 16:30:15 -0700

    Hardcode Condition Possibilities, Adjust styling, Add location

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 // import 'dotenv/config';
 
-const countryName = document.querySelector('.country');
-const countryNameSpan = document.querySelector('.country-data');
-const cityName = document.querySelector('.city');
-const cityNameSpan = document.querySelector('.city-data')
-const localTime = document.querySelector('.local-time');
-const localTimeSpan = document.querySelector('.local-time-data')
-const currentCond = document.querySelector('.current-cond');
-const currentCondSpan = document.querySelector('.current-cond-data')
-const currentTemp = document.querySelector('.current-temp');
-const currentTempSpan = document.querySelector('.current-temp-data')
-const humidity = document.querySelector('.humidity');
-const humiditySpan = document.querySelector('.humidity-data')
-const windSpeed = document.querySelector('.wind-speed');
-const windSpeedSpan = document.querySelector('.wind-speed-data')
-const precipitation = document.querySelector('.precipitation');
-const precipitationSpan = document.querySelector('.precipitation-data')
-
-const location = document.querySelector('.location');
+const place = document.querySelector('.location');
 const time = document.querySelector('.time');
 const temp = document.querySelector('.temp');
 const condition = document.querySelector('.condition-text');
@@ -30,6 +13,58 @@ const vis = document.querySelector('.vis-data');
 
 const search = document.getElementById('search');
 
+const conditionDict = {
+	"Sunny": "./images/Sunny.png",
+  "Clear": "./images/Sunny.png",
+	"Partly cloudy": "./images/Partly Cloudy.png",
+  "Cloudy": "./images/Cloudy.png",
+  "Overcast": "./images/Cloudy.png",
+  "Mist": "./images/Fog.png",
+  "Patchy rain possible": "./images/Rain.png",
+  "Patchy snow possible": "./images/Snow.png",
+  "Patchy sleet possible": "./images/Snow.png",
+  "Patchy freezing drizzle possible": "./images/Sprinkle.png",
+  "Thundery outbreaks possible": "./images/Lightning.png",
+  "Blowing snow": "./images/Snow.png",
+  "Blizzard": "./images/Snow.png",
+  "Fog": "./images/Fog.png",
+  "Freezing fog": "./images/Fog.png",
+  "Patchy light drizzle": "./images/Sprinkle.png",
+  "Light drizzle": "./images/Sprinkle.png",
+  "Freezing drizzle": "./images/Rain.png",
+  "Heavy freezing drizzle": "./images/Heavy Rain.png",
+  "Patchy light rain": "./images/Rain.png",
+  "Light rain": "./images/Rain.png",
+  "Moderate rain at times": "./images/Rain.png",
+  "Moderate rain": "./images/Rain.png",
+  "Heavy rain at times": "./images/Heavy Rain.png",
+  "Heavy rain": "./images/Heavy Rain.png",
+  "Light freezing rain": "./images/Rain.png",
+  "Moderate or heavy freezing rain": "./images/Heavy Rain.png",
+  "Light sleet": "./images/Snow.png",
+  "Moderate or heavy sleet": "./images/Snow.png",
+  "Patchy light snow": "./images/Snow.png",
+  "Light snow": "./images/Snow.png",
+  "Patchy moderate snow": "./images/Snow.png",
+  "Moderate snow": "./images/Snow.png",
+  "Patchy heavy snow": "./images/Snow.png",
+  "Heavy snow": "./images/Snow.png",
+  "Ice pellets": "./images/Snow.png",
+  "Light rain shower": "./images/Rain.png",
+  "Moderate or heavy rain shower": "./images/Heavy Rain.png",
+  "Torrential rain shower": "./images/Rain.png",
+  "Light sleet showers": "./images/Snow.png",
+  "Moderate or heavy sleet showers": "./images/Snow.png",
+  "Light snow showers": "./images/Snow.png",
+  "Moderate or heavy snow showers": "./images/Snow.png",
+  "Light showers of ice pellets": "./images/Snow.png",
+  "Moderate or heavy showers of ice pellets": "./images/Snow and Rain.png",
+  "Patchy light rain with thunder": "./images/Lightning.png",
+  "Moderate or heavy rain with thunder": "./images/Lightning.png",
+  "Patchy light snow with thunder": "./images/Lightning.png",
+	"Moderate or heavy snow with thunder": "./images/Lightning.png"
+}
+
 async function getData(location) {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=85cbece73a8846868d5174556230607&q=${location}`, { mode: "cors" });
   const weatherData = await response.json();
@@ -40,10 +75,11 @@ async function getData(location) {
   const currTime = `
     ${hour <= 12 ? hour : hour - 12}:${minute} ${hour < 12 ? "am" : "pm"}
   `
+  place.textContent = `${weatherData.location.name}, ${weatherData.location.region}, ${weatherData.location.country}`
   time.textContent = currTime
   temp.textContent = `${weatherData.current.temp_f}\u{00B0}F`
   condition.textContent = `${weatherData.current.condition.text}`
-  conditionImg.src = `./images/${weatherData.current.condition.text}.png`
+  conditionImg.src = conditionDict[weatherData.current.condition.text];
   humid.textContent = `${weatherData.current.humidity} %`
   wind.textContent = `${weatherData.current.wind_mph} mph`
   precip.textContent = `${weatherData.current.precip_in} in`
@@ -54,25 +90,7 @@ async function getData(location) {
           "Extreme"})`
   vis.textContent = `${weatherData.current.vis_miles} mi`
 
-  countryName.textContent = "Country: "
-  countryNameSpan.textContent = weatherData.location.country
-  cityName.textContent = "City: "
-  cityNameSpan.textContent = weatherData.location.name
-  localTime.textContent = "Local time: "
-  localTimeSpan.textContent = `
-    ${hour <= 12 ? hour : hour - 12}:${minute} ${hour < 12 ? "am" : "pm"}`
-  currentCond.textContent = "Current conditions: "
-  currentCondSpan.textContent = weatherData.current.condition.text
-  currentTemp.textContent = "Current temp: "
-  currentTempSpan.textContent = `${weatherData.current.temp_f}°F`
-  humidity.textContent = "Humidity: "
-  humiditySpan.textContent = `${weatherData.current.humidity}%`
-  windSpeed.textContent = "Wind speed: "
-  windSpeedSpan.textContent = `${weatherData.current.wind_mph}mph`
-  precipitation.textContent = "Precipitation: "
-  precipitationSpan.textContent = `${weatherData.current.precip_in}in`
-
-  return weatherData
+ weatherData
 }
 
 search.value = "los angeles"
